d5d2203d249286450f2f579ee6315eb77fa61245
max-jellycat <max@jellycat.fr>
2020-10-06 02:21:02 +0200

    layout ok

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Destini',
       theme: ThemeData(
+        fontFamily: 'RobotoSlab',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
@@ -21,6 +22,9 @@ class App extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(child: StoryPage()),
         ),
       ),
     );
@@ -35,6 +39,56 @@ class StoryPage extends StatefulWidget {
 class _StoryPageState extends State<StoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Text(
+              'Story text will go here.',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {},
+              color: Colors.red,
+              child: Text(
+                'Choice 1',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text(
+                'Choice 2',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
